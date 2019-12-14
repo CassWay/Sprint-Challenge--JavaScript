ec62fbd5f7dffd2e45d213b9785cc09b061a7d5d
Wayne Cassity <webdev.cassity@gmail.com>
2019-12-13 19:11:20 -0800

    completed objects-array and Stretch;

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -133,8 +133,14 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
-console.log(displayNames);
+displayNamesSci = (zooAnimals) => {
+  let displayNames = [];
+    zooAnimals.forEach((arrayItem) => {
+      displayNames.push(`Name: ${arrayItem.animal_name}, Scientific: ${arrayItem.scientific_name}`)
+    })
+    return displayNames ;
+  }
+  console.log(displayNamesSci(zooAnimals));
 
 /* Request 2: .map()
 
@@ -142,24 +148,37 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
-console.log(lowCaseAnimalNames);
+lowerCaseNamesList = (zooAnimals) => {
+  return zooAnimals.map((arrayItem) => {
+    return arrayItem.animal_name.toLowerCase()
+  })
+}
+
+console.log(lowerCaseNamesList(zooAnimals));
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
-console.log(lowPopulationAnimals);
+lowPopulationAnimals = (zooAnimals, endangered) => {
+  return zooAnimals.filter((arrayItems) => {
+    return arrayItems.population < endangered ;
+  })
+}
+console.log(lowPopulationAnimals(zooAnimals, 5));
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+populationTotal = (zooAnimals) => {
+  return zooAnimals.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.population
+  }, 0)
+}
+console.log(populationTotal(zooAnimals));
 
 
 /*
